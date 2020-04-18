d2d8d570993a07d5f3d1d2f4f7c4c3b1d2277869
Joe Auby <joeyauby@gmail.com>
2020-04-18 14:18:48 -0700

    player movement

---
+++ unity/rona/Assets/Scripts/InputHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    float inputX;
+    float inputY;
+
+    void Start()
+    {
+
+    }
+
+    public float getInputX() {
+        return inputX;
+    }
+
+    public float getInputY() {
+        return inputY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //dashPressed = Input.GetButtonDown("Dash") ? true : false;
+        inputX = Input.GetAxis("Horizontal");
+        inputY = Input.GetAxis("Vertical");
+    }
+}
---
+++ unity/rona/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    InputHandler input;
+    Rigidbody2D rigidBody;
+    public float velocity;
+
+    void Start()
+    {
+        input = GetComponent<InputHandler>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var inputY = input.getInputY();
+        var inputX = input.getInputX();
+        if (inputX == 0 && inputY == 0){
+            rigidBody.velocity = new Vector2(0, 0);
+            return;
+        }
+        var angle = Mathf.Atan2(input.getInputY(),  input.getInputX());
+        var velX = Mathf.Cos(angle) * velocity;
+        var velY = Mathf.Sin(angle) * velocity;
+
+
+
+        Debug.Log(angle);
+        //Debug.Log("VelY: " + input.getInputY() + " VelX: " + input.getInputX());
+
+        rigidBody.velocity = new Vector2(velX, velY);
+        //Time.deltaTime;
+        
+    }
+}
