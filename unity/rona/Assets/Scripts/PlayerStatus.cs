6669fc608436d6ff301cba86bb1f7e7ee3801c37
alexander <alexander.ressler@gmail.com>
2020-04-18 23:34:37 -0400

    Adding a status bar

---
+++ unity/rona/Assets/Scripts/PlayerStatus.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerStatus : MonoBehaviour
+{
+
+    public float maxHealth;
+
+    public GameObject healthStatusBarGo;
+
+    [Serializable]
+    public class HealthChangedEvent : UnityEvent<float> { }
+    public HealthChangedEvent healthChanged;
+
+
+    public void SetHealth(float value) {
+        healthChanged.Invoke(value);
+    }
+    
+
+    // Start is called before the first frame update
+    void Start() {
+        healthStatusBarGo.GetComponent<UIStatusBar>().maxValue = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        SetHealth(Time.time % maxHealth);
+    }
+}
---
+++ unity/rona/Assets/Scripts/UIStatusBar.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIStatusBar : MonoBehaviour
+{
+
+    public enum StatusBarType {
+        Horizontal,
+        Vertical, 
+    }
+
+    public StatusBarType layoutType;
+
+    public RectTransform bar;
+
+    public float maxValue;
+    private float value;
+
+    // Start is called before the first frame update
+    void Start() {
+        if (maxValue == 0) {
+            maxValue = 1;
+        }
+        switch(layoutType) {
+            case StatusBarType.Horizontal:
+                bar.pivot = new Vector2(0f, 0.5f);
+                break;
+            case StatusBarType.Vertical:
+                bar.pivot = new Vector2(0.5f, 0f);
+                break;
+        }
+    }
+
+
+    public void setValue(float value) {
+        SetPercent(value / maxValue);
+    }
+
+
+    /// <summary>
+    /// Pass a value between 0 and 1 to set the height of the indicator bar.
+    /// </summary>
+    /// <param name="percent"></param>
+    public void SetPercent(float percent) {
+        switch(layoutType) {
+            case StatusBarType.Horizontal:
+                bar.localScale = new Vector3(percent, 1f, 1f);
+                break;
+            case StatusBarType.Vertical:
+                bar.localScale = new Vector3(1f, percent, 1f);
+                break;
+        }
+    }
+
+}
