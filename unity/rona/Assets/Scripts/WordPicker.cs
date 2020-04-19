2765b314a3d999ce6bbd4c7d072b1c375a78dc66
Joe Auby <joeyauby@gmail.com>
2020-04-18 18:56:28 -0700

    word picker loading the json file

---
+++ unity/rona/Assets/Scripts/WordPicker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class WordPicker : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    
+    public JsonWords words;
+    void Start()
+    {
+        words = JsonUtility.FromJson<JsonWords>(jsonFile.text);
+
+        Debug.Log(words.letters9[10]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
+[System.Serializable]
+public class JsonWords {
+    public string[] letters3;
+    public string[] letters4;
+    public string[] letters5;
+    public string[] letters6;
+    public string[] letters7;
+    public string[] letters8;
+    public string[] letters9;
+    public string[] letters10;
+    public string[] letters11;
+    public string[] letters12;
+    public string[] letters13;
+    public string[] letters14;
+    public string[] letters15;
+}
