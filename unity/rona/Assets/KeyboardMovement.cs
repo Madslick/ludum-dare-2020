8ba3d03ffbc6d024b9761701938cd67642161e9d
Harjit <harjit@imme.rs>
2020-04-18 18:49:19 -0700

    added trails to bullet

---
+++ unity/rona/Assets/KeyboardMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    Rigidbody2D rigidBody;
+    public float speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 velocity = new Vector2();
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            velocity.y = speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            velocity.y = -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            velocity.x = -speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            velocity.x = speed * Time.deltaTime;
+        }
+
+        rigidBody.velocity = velocity;
+    }
+}
